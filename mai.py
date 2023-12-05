bc0aec56ed2a0637a10746fa91bb5a964f1ffe57
Suraj  Shelar <83068425+thesurajsh@users.noreply.github.com>
2023-12-05 18:31:52 +0530

    main.py

---
+++ mai.py
@@ -0,0 +1,14 @@
+
+#Age detector project by using python
+
+print("welcome to my age program")
+age=int(input("enter your age"))
+
+if age<=16:
+    print("Now your children ")
+elif age>=17:
+    print("Now your mature or adult ")
+elif age>=31:
+    print("Now your middele-aged adult ")
+elif age>=45:
+    print("Now your Old aged adult ")
